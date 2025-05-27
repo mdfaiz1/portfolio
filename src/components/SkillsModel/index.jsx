38e66e332b981935825c5c48e7f6a5fc21eec20b
mdfaiz9 <mdfaizalam827@gmail.com>
2025-05-27 16:57:12 +0530

    fix: change class to className in SkillsModel component for JSX compatibility

--- src/components/SkillsModel/index.jsx
+++ src/components/SkillsModel/index.jsx
@@ -13,21 +13,22 @@ const SkillsModel = () => {
     { icon: "<FiEdit />", color: "orange", label: "Notes" },
     { icon: "<FiBarChart2 />", color: "green", label: "Stats" },
   ];
-
   const [open, setOpen] = React.useState(false);
   return (
     <>
       <button
-        class="border text-gray-50  duration-300 relative group cursor-pointer   overflow-hidden h-12 w-35 rounded-md bg-neutral-800 p-2  font-extrabold hover:bg-sky-700"
+        className="border text-gray-50  duration-300 relative group cursor-pointer   overflow-hidden h-12 w-35 rounded-md bg-neutral-800 p-2  font-extrabold hover:bg-sky-700"
         variant="outlined"
         color="neutral"
         onClick={() => setOpen(true)}
       >
-        <div class="absolute group-hover:-top-1 group-hover:-right-2 z-10 w-16 h-16 rounded-full group-hover:scale-150  duration-700 right-12 top-12 bg-yellow-500"></div>
-        <div class="absolute group-hover:-top-1 group-hover:-right-2 z-10 w-12 h-12 rounded-full group-hover:scale-150  duration-700 right-20 -top-6 bg-orange-500"></div>
-        <div class="absolute group-hover:-top-1 group-hover:-right-2 z-10 w-8 h-8   rounded-full group-hover:scale-150  duration-700 right-32 top-6 bg-pink-500"></div>
-        <div class="absolute group-hover:-top-1 group-hover:-right-2 z-10 w-4 h-4   rounded-full group-hover:scale-150  duration-700 right-2 top-12 bg-red-600"></div>
-        <p class="z-10 text-xl absolute bottom-2 left-2 font-bold">SKILLS</p>
+        <div className="absolute group-hover:-top-1 group-hover:-right-2 z-10 w-16 h-16 rounded-full group-hover:scale-150  duration-700 right-12 top-12 bg-yellow-500"></div>
+        <div className="absolute group-hover:-top-1 group-hover:-right-2 z-10 w-12 h-12 rounded-full group-hover:scale-150  duration-700 right-20 -top-6 bg-orange-500"></div>
+        <div className="absolute group-hover:-top-1 group-hover:-right-2 z-10 w-8 h-8   rounded-full group-hover:scale-150  duration-700 right-32 top-6 bg-pink-500"></div>
+        <div className="absolute group-hover:-top-1 group-hover:-right-2 z-10 w-4 h-4   rounded-full group-hover:scale-150  duration-700 right-2 top-12 bg-red-600"></div>
+        <p className="z-10 text-xl absolute bottom-2 left-2 font-bold">
+          SKILLS
+        </p>
       </button>
       <Modal
         aria-labelledby="modal-title"
